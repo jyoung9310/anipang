4637e67dad2e1818677cf432a2813b178433afb3
Jiyoung Yoon <jyoung9310@naver.com>
2015-12-03 20:27:23 +0900

    stack size

--- anipang/anipang/PangStack.h
+++ anipang/anipang/PangStack.h
@@ -1,10 +1,11 @@
 #pragma once
 
+#define SIZEOFSTACK 100
 class PangStack {
-	int row;
-	int col;
-	int direction;
-	int count;
+	int row[SIZEOFSTACK];
+	int col[SIZEOFSTACK];
+	int direction[SIZEOFSTACK];
+	int count[SIZEOFSTACK];
 public:
 	void push(int, int, int, int);
 	int* pop();
