9e0e519d8db0b2a71e87ea64b820dee45cbe57ed
hea1432 <hea1432@naver.com>
2015-12-03 21:18:48 +0900

    PangStackUpdate

--- anipang/anipang/PangStack.cpp
+++ anipang/anipang/PangStack.cpp
@@ -1,2 +1,22 @@
 #pragma once
-#include "PangStack.h"
+#include "PangStack.h"	
+
+
+void PangStack::push(int rowinput, int colinput, int directioninput, int countinput) {
+	row[pushnum] = rowinput;
+	col[pushnum] = colinput;
+	direction[pushnum] = directioninput;
+	count[pushnum] = countinput;
+
+	pushnum++;
+}
+
+int* PangStack::pop() {
+
+	row[pushnum] = '\0';
+	col[pushnum] = '\0';
+	direction[pushnum] = '\0';
+	count[pushnum] = '\0';
+
+	pushnum--;
+}
--- anipang/anipang/PangStack.h
+++ anipang/anipang/PangStack.h
@@ -6,7 +6,9 @@ class PangStack {
 	int col[SIZEOFSTACK];
 	int direction[SIZEOFSTACK];
 	int count[SIZEOFSTACK];
+	
 public:
-	void push(int, int, int, int);
+	void push(int rowinput, int colinput, int directioninput, int countinput);
 	int* pop();
+	int pushnum = 0;
 };
